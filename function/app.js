1b40d341657cbd43fb8b510d7f610aeca8286a8a
Moinak <moinak.dey8@gmail.com>
2023-01-08 21:36:32 +0530

    disabled right click and view page source

--- function/app.js
+++ function/app.js
@@ -1,5 +1,22 @@
 gsap.registerPlugin(Flip);
 
+document.onkeydown = function (e) {
+    e = e || window.event;
+    if (e.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
+
+document.addEventListener('contextmenu', event => event.preventDefault());
 
 
 // / Selectors 
